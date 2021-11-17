938d9d86cbc950d767671a2ad03b2d969024ae3c
Juan Nates <nates1999123@gmail.com>
2021-11-16 23:04:08 -0500

    new-added-connection-db

---
+++ src/database/db.ts
@@ -0,0 +1,21 @@
+import { Mongoose } from "mongoose";
+import { MpathPlugin } from 'mongoose-mpath';
+
+const LocationSchema = new Mongoose.Schema({name: String});
+LocationSchema.plugin(MpathPlugin);
+
+const LocationModel = Mongoose.model('Location', LocationSchema);
+
+const europe = new LocationModel({name: 'europe'});
+const sweden = new LocationModel({name: 'sweden', parent: europe});
+const stockholm = new LocationModel({name: 'stockholm', parent: sweden});
+
+await europe.save();
+await sweden.save();
+await stockholm.save();
+
+//const url = 'mongodb://localhost/base_admin'
+//Mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+//const connection = Mongoose.connection
+
+//module.exports = connection
